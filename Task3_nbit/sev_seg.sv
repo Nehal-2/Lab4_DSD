dac03f8c58fa34f83f9673ae86442f3c929fcb92
Nehal <NehalAlsuhaibani@gmail.com>
2024-11-18 17:18:10 +0300

    Task 3 - nbit register & FPGA Implementation

---
+++ Task3_nbit/sev_seg.sv
@@ -0,0 +1,61 @@
+module register_nbit_top(
+    input wire CLK100MHZ,    // using the same name as pin names
+    input wire CPU_RESETN,   
+    output wire CA, CB, CC, CD, CE, CF, CG, DP,
+    input wire BTNC,
+    output wire [7:0] AN,    
+    input wire [15:0] SW     
+);
+
+
+logic reset_n;
+logic clk;
+
+assign reset_n = CPU_RESETN;
+assign clk = CLK100MHZ;
+
+
+// Seven segments Controller
+wire [6:0] Seg;
+wire [3:0] digits[0:7];
+
+logic [7:0] q;
+
+register_nbit #(.n(8))reg_inst(
+        .clk(BTNC),
+        .areset(CPU_RESETN),
+        .en(SW[8]),
+        .d(SW[7:0]),
+        .q(q)
+    );
+
+assign digits[0] = q[3:0];
+assign digits[1] = q[7:4];
+assign digits[2] = 4'b1111;
+assign digits[3] = 4'b1111;
+assign digits[4] = 4'b1111;
+assign digits[5] = 4'b1111;
+assign digits[6] = 4'b1111;
+assign digits[7] = 4'b1111;
+
+
+sev_seg_controller ssc(
+    .clk(clk),
+    .resetn(reset_n),
+    .digits(digits),
+    .Seg(Seg),
+    .AN(AN)
+);
+
+
+assign CA = Seg[0];
+assign CB = Seg[1];
+assign CC = Seg[2];
+assign CD = Seg[3];
+assign CE = Seg[4];
+assign CF = Seg[5];
+assign CG = Seg[6];
+assign DP = 1'b1; // turn off the dot point on seven segs
+
+
+endmodule : register_nbit_top
